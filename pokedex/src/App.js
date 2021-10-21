8fe2e65f232a56ceb77c9c8aee211a5d78bbc278
Glauber Apolinario de Freitas <glauber.apolinario@yahoo.com.br>
2021-10-20 21:42:02 -0300

    Adição da pagina de detalhes do pokemon

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,8 +2,16 @@ import './App.css';
 import React from 'react';
 import Route from './Routes/Route';
 import Header from './components/Header';
+import { useEffect } from 'react/cjs/react.development';
 
 const App = () => {
+  //const [listaPokemon, setListaPokemon] = useState([])
+  //const [pokedex, setPokedex] = useState([])
+
+  useEffect(()=>{
+    //busca dados na api
+    // seta os dados no estado com setLista
+  },[])
   return (
     <div>
       <Header />
